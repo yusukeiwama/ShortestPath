8831222c1e9f2c67f225a6d8bc91314d0dafc529
Yusuke Iwama <yusuke.iwama@gmail.com>
2013-12-13 01:58:45 +0900

    add TSP class

---
+++ ShortestPath/USKTSP.h
@@ -0,0 +1,13 @@
+//
+//  USKTSP.h
+//  ShortestPath
+//
+//  Created by Yusuke Iwama on 12/13/13.
+//  Copyright (c) 2013 Yusuke Iwama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface USKTSP : NSObject
+
+@end
---
+++ ShortestPath/USKTSP.m
@@ -0,0 +1,13 @@
+//
+//  USKTSP.m
+//  ShortestPath
+//
+//  Created by Yusuke Iwama on 12/13/13.
+//  Copyright (c) 2013 Yusuke Iwama. All rights reserved.
+//
+
+#import "USKTSP.h"
+
+@implementation USKTSP
+
+@end
